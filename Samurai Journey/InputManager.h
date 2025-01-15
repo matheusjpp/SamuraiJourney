d6f559fee818e571825260ccdca2008ddf1ee05e
danilorubio <felipedanilo811@gmail.com>
2025-01-15 18:20:07 -0300

    eventsManager,input(incompleto),observer(incompleto)

---
+++ Samurai Journey/InputManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Observer.h"
+namespace Managers {
+	class InputManager
+	{
+	private:
+		std::list<Observer> observer;
+		static InputManager* pInput;
+		InputManager();
+	public:
+		static InputManager* getInstance();
+		~InputManager();
+		void Attach();
+		void Detach();
+		void notify();
+	};
+}
---
+++ Samurai Journey/Observer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Platform.h"
+class Observer
+{
+private:
+	Managers::InputManager* pIm;
+public:
+	Observer();
+	~Observer();
+	virtual void notifyKeyPressed() = 0;
+	virtual void notifyKeyReleased() = 0;
+};
+
